fef127d5ec66e1165bb51b934b3c5782a451cb6a
frkntony <tony.elistratov@gmail.com>
2018-07-24 22:54:01 +0300

    api connected to parse data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.setState = {
+      type: 'meat-and-filler',
+      paras: 5,
+      lorem: 1,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount(){
+    this.getSampleText()
+  }
+
+  getSampleText = () => {
+    axios.get(`https://baconipsum.com/api/?type=${this.type}`)
+    .then( data => console.log(data) )
+    .catch( e => console.log(e) )
+  }
+
   render() {
     return (
       <div className="App">
