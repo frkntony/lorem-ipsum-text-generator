ff94202afcd8832316deac1c04d9e0623332224f
frkntony <tony.elistratov@gmail.com>
2018-07-25 21:28:31 +0300

    all functionality implemented. basic styling added.

--- src/App.js
+++ src/App.js
@@ -4,31 +4,72 @@ import axios from 'axios'
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.setState = {
+    this.state = {
+      apiURL: 'https://baconipsum.com/api/',
       type: 'meat-and-filler',
       paras: 5,
       lorem: 1,
-      html: true,
       text: ''
     }
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.getSampleText()
   }
 
+  // full api sample call
+  //?type=all-meat&paras=3&start-with-lorem=1&format=html
   getSampleText = () => {
-    axios.get(`https://baconipsum.com/api/?type=${this.type}`)
-    .then( data => console.log(data) )
-    .catch( e => console.log(e) )
+
+    axios.get(`${this.state.apiURL}?type=${this.state.type}?paras=${this.state.paras}
+                ?start-with-lorem=${this.state.lorem}`)
+      .then(res => {
+        this.setState({ text: res.data })
+        console.log(res.data)
+      })
+      .catch(e => console.log(e))
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Create React App</h1>
+        <div className='container-fluid'>
+          <form>
+
+            <div className='form-group'>
+              <label htmlFor='paras'>How many paragraphs do you need?</label>
+              <select className='form-control' id='paras'>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+                <option value='8'>8</option>
+                <option value='9'>9</option>
+                <option value='10'>10</option>
+              </select>
+            </div>
+
+            <div className='form-group'>
+              <div className='row'>
+                <div className='col-12'>
+                  {this.state.text}
+                </div>
+              </div>
+            </div>
+
+            <div className='form-row text-center'>
+              <div className='col-12'>
+                <button className='btn btn-primary center'>Generate</button>
+              </div>
+            </div>
+          </form>
+        </div>
+
       </div>
     );
   }
